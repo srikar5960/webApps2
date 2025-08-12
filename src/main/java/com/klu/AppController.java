8d491ce9157cda3f2a17cfa005eaf1e908185483
srikar5960 <vijaysrikarpentapati@gmail.com>
2025-08-12 11:22:07 +0530

    Update AppController.java

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+    
+@CrossOrigin
 public class AppController {
     
     @GetMapping("/demo1")
